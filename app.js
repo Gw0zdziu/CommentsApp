8d75f51e1360e2f4a0f4f2a95af1b9859eca3228
Przemysław Gwóźdź <przemyslaw.gwozdz@ibcs.pl>
2022-09-08 13:34:45 +0200

    Add own comments

--- app.js
+++ app.js
@@ -1,94 +1,90 @@
 const containerComments = document.querySelector('.container-comments')
-const newCommentTemplate = document.querySelector('.new-comment')
+let newCommentTemplate = document.querySelector('.new-comment')
 const replyButton = document.querySelector('.button--reply')
 const editButton = document.querySelector('.button--edit')
 const deleteButton = document.querySelector('.button--delete')
+const sendButton = document.querySelector('.button--send')
+let textareaAddComment = document.querySelector('.textarea')
 
-const fragment = document.createDocumentFragment()
+let fragment = document.createDocumentFragment
+
 let dataComments = {}
 
-const loadData = async ()=>{
-    let localData = localStorage.getItem('comments')
-    if(localData){
-        const data = JSON.parse(localData)  
-        dataComments.comments = data.comments
-        dataComments.currentUser = data.currentUser
-        currentUser = dataComments.currentUser;    
-        displayComments(dataComments.comments)
-    } else{
-        const response = await fetch('./data.json')
-        const data = await response.json()
-        localStorage.setItem('comments', JSON.stringify(data))
-        location.reload
-    }
+onload = async () => {
+	let localData = localStorage.getItem('comments')
+	if (localData) {
+		const data = JSON.parse(localData)
+		dataComments.comments = data.comments
+		dataComments.currentUser = data.currentUser
+		currentUser = dataComments.currentUser
+		displayComments(dataComments.comments)
+	} else {
+		const response = await fetch('./data.json')
+		const data = await response.json()
+		localStorage.setItem('comments', JSON.stringify(data))
+		location.reload
+	}
 }
 
-const displayButtons = (buttonView)=>{
-    if(buttonView){
-        editButton.classList.toggle('display-button')
-        deleteButton.classList.toggle('display-button')
-    }else{
-        replyButton.classList.toggle('display-button')
-    }
+const displayButtons = (buttonView) => {
+	if (buttonView) {
+		replyButton.classList.add('display-button')
+	} else {
+		editButton.classList.add('display-button')
+		deleteButton.classList.add('display-button')
+	}
 }
 
-const displayComments =  (comments)=>{
-    console.log(comments);
-    comments.map(comment => {
-        if(comment.user.username === dataComments.currentUser.username){
-             
-            templateComment(newCommentTemplate, comment, comment.id)
-        }else{
-            displayButtons(false)
-            templateComment(newCommentTemplate, comment, comment.id)
-        }
-    })
-    containerComments.appendChild(fragment)
+const displayComments = (comments) => {
+	comments.map((comment) => {
+		if (comment.user.username === dataComments.currentUser.username) {
+			displayButtons(false)
+			templateComment(newCommentTemplate, comment)
+		} else {
+			displayButtons(false)
+			templateComment(newCommentTemplate, comment)
+		}
+	})
+	containerComments.appendChild(newCommentTemplate)
 }
 
-
-const templateComment = (template, data, id)=>{
-        template.querySelector('.sentence-comment').textContent = data.content
-        template.querySelector('.created-date').innerHTML = data.createdAt
-        template.querySelector('.score').textContent = data.score
-        template.querySelector('.image').src = data.user.image.png
-        template.querySelector('.user-name').textContent = data.user.username
-        template.querySelector('.image').textContent = data.user.image.png
-        let clone = template.cloneNode(true)
-        fragment.appendChild(clone)
+const templateComment = (template, data) => {
+	const {
+		id,
+		content,
+		createdAt,
+		score,
+		user: {
+			image: { png },
+			username,
+		},
+	} = data
+	template.setAttribute('id', id)
+	template.querySelector('.sentence-comment').textContent = content
+	template.querySelector('.created-date').innerHTML = createdAt
+	template.querySelector('.score').textContent = score
+	template.querySelector('.image').src = png
+	template.querySelector('.user-name').textContent = username
+	newCommentTemplate = template.cloneNode(true)
 }
 
-loadData()
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+sendButton.addEventListener('click', () => {
+	let idLastComment = containerComments.lastElementChild.getAttribute('id')
+	console.log(idLastComment)
+	const dataTemplateComment = {
+		id: ++idLastComment,
+		content: textareaAddComment.value,
+		createdAt: 'Now',
+		score: 0,
+		user: {
+			image: {
+				png: dataComments.currentUser.image.png,
+				webp: dataComments.currentUser.image.webp,
+			},
+			username: dataComments.currentUser.username,
+		},
+	}
+	const clone = newCommentTemplate.cloneNode(true)
+	templateComment(clone, dataTemplateComment)
+	containerComments.appendChild(clone)
+})
