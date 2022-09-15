3c42729501f2cbd32335de1dbccef9bb1233d3dc
Przemysław Gwóźdź <przemyslaw.gwozdz@ibcs.pl>
2022-09-15 15:11:09 +0200

    Add delete comment and confirm delete or cancel

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const container = document.querySelector('.comments')
 const commentTemplate = document.querySelector('#new-comment-template').content
 const commentReplyTemplate = document.querySelector('#new-comment-reply-template').content
 const buttonSend = document.querySelector('.button--send')
-
+const modalContainer = document.querySelector('.modal-container')
 
 const comment = document.createDocumentFragment()
 
@@ -20,64 +20,74 @@ onload = async () => {
 		const response = await fetch('./data.json')
 		const data = await response.json()
 		localStorage.setItem('comments', JSON.stringify(data))
-		location.reload
+		location.reload()
 	}
 }
 
-
-
 const createComments = (data) => {
-	data.forEach(item => {
-		if(item.user.username !== dataComments.currentUser.username){
+	data.forEach((item) => {
+		if (item.user.username !== dataComments.currentUser.username) {
 			getTemplateComment(commentTemplate, item, false)
-		}else{
+		} else {
 			getTemplateComment(commentTemplate, item, true)
 		}
-
-	});
+	})
 	container.appendChild(comment)
 }
 
-
-const getTemplateComment =  (template, data, viewButtons)=>{
+const getTemplateComment = (template, data, viewButtons) => {
 	template.querySelector('.new-comment').setAttribute('id', data.id)
 	template.querySelector('.score').textContent = data.score
 	template.querySelector('.image').src = data.user.image.png
 	template.querySelector('.user-name').textContent = data.user.username
 	template.querySelector('.created-date').textContent = data.createdAt
 	template.querySelector('.sentence-comment').textContent = data.content
-	if(viewButtons === true){
+	if (viewButtons) {
 		template.querySelector('.button--reply').style.display = 'none'
 		template.querySelector('.button--edit').style.display = 'flex'
 		template.querySelector('.button--delete').style.display = 'flex'
-	}else if(viewButtons === false){
+	} else {
 		template.querySelector('.button--reply').style.display = 'flex'
 		template.querySelector('.button--edit').style.display = 'none'
 		template.querySelector('.button--delete').style.display = 'none'
 	}
 	let cloneTemplate = template.cloneNode(true)
-	comment.appendChild(cloneTemplate)		
+	comment.appendChild(cloneTemplate)
 }
 
 const addCommment = () => {
-	let contentTextArea = document.querySelector('.textarea').value
-	let contextOwnComment = {
-	  "id" : 1,
-      "content": contentTextArea,
-      "createdAt": "1 month ago",
-      "score": 0,
-      "user": {
-        "image": { 
-          "png": dataComments.currentUser.image.png,
-          "webp": dataComments.currentUser.image.webp,
-        },
-        "username": dataComments.currentUser.username
-      },
+	let textAreaElement = document.querySelector('.textarea')
+	let contentTextArea = textAreaElement.value
+	if (contentTextArea) {
+		let contextOwnComment = {
+			id: 1,
+			content: contentTextArea,
+			createdAt: '1 month ago',
+			score: 0,
+			user: {
+				image: {
+					png: dataComments.currentUser.image.png,
+					webp: dataComments.currentUser.image.webp,
+				},
+				username: dataComments.currentUser.username,
+			},
+		}
+		getTemplateComment(commentTemplate, contextOwnComment, true)
+		container.appendChild(comment)
+		textAreaElement.value = ''
+	} else {
+		textAreaElement.setAttribute('placeholder', 'Insert content comment')
 	}
-	getTemplateComment(commentTemplate, contextOwnComment, true)
-	container.appendChild(comment)
 }
 
-const deletComment = (e)=>{
-	console.log(e.parentNode.remove());
+const deleteComment = (event) => {
+	modalContainer.classList.add('display-modal')
+	let deleteCommentButtton = modalContainer.querySelector('.modal-confirm-button')
+	deleteCommentButtton.addEventListener('click', () => {
+		event.parentNode.parentNode.remove()
+		modalContainer.classList.remove('display-modal')
+	})
+}
+const cancelDeleteComment = () => {
+	modalContainer.classList.remove('display-modal')
 }
